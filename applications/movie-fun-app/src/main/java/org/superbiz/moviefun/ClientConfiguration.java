69988a05817c27bcca75eb2acd0c00719a9da0ef
pal_user <reply2ppg@yahoo.com>
2020-01-29 11:28:32 -0700

    secure our services

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/ClientConfiguration.java
@@ -4,20 +4,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
 import org.superbiz.moviefun.albumsapi.AlbumsClient;
 import org.superbiz.moviefun.moviesapi.MoviesClient;
 
 @Configuration
 public class ClientConfiguration {
 
-    @Value("${albums.url}") String albumsUrl;
-    @Value("${movies.url}") String moviesUrl;
-
-    @Bean
-    public RestOperations restOperations() {
-        return new RestTemplate();
-    }
+    @Value("${albums.url}")
+    String albumsUrl;
+    @Value("${movies.url}")
+    String moviesUrl;
 
     @Bean
     public AlbumsClient albumsClient(RestOperations restOperations) {
