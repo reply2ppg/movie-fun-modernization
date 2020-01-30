6ae4f451a6c71bafa8fa208b551485ff9b31242a
pal_user <reply2ppg@yahoo.com>
2020-01-30 15:11:00 -0700

    move albums to kotlin

---
+++ components/albums/src/main/java/org/superbiz/moviefun/CsvUtils.kt
@@ -0,0 +1,22 @@
+package org.superbiz.moviefun
+
+import com.fasterxml.jackson.databind.ObjectReader
+import java.io.IOException
+import java.io.InputStream
+import java.util.*
+
+object CsvUtils {
+
+    @Throws(IOException::class)
+    fun <T> readFromCsv(objectReader: ObjectReader, inputStream: InputStream): List<T> {
+        val results = ArrayList<T>()
+
+        val iterator = objectReader.readValues<T>(inputStream)
+
+        while (iterator.hasNext()) {
+            results.add(iterator.nextValue())
+        }
+
+        return results
+    }
+}
